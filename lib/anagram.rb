ad53ab77ac3ce805de7d9e55c49f3d164c5beaec
NicMortelliti <npmortelliti@gmail.com>
2022-05-27 15:59:07 -0700

    Assignment complete

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,21 @@
-# Your code goes here!
+class Anagram
+  def initialize(word)
+    @word = word
+  end
+
+  def match(array)
+    return_array = []
+    
+    array.each do |array_word|
+      if array_word.chars.sort == @word.chars.sort
+        return_array << array_word
+      end
+    end
+
+    return_array
+  end
+
+end
+
+listen = Anagram.new("listen")
+listen.match(%w[enlists google inlets banana])
